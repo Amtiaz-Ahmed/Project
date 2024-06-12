9d5d82fca75881d687a29032e2f02974f341e385
Amtiaz-Ahmed <amtiazahmed797@gmail.com>
2024-06-12 14:20:50 +0500

    database connection completed

---
+++ index.js
@@ -0,0 +1,26 @@
+const express           = require('express');
+const app               = express();
+const path              = require('path');
+const cookieParser      = require('cookie-parser');
+
+const db                = require('./config/mongoose-connection');
+const usersRouter       = require('./routes/usersRouter');
+const ownersRouter      = require('./routes/ownersRouter');
+const productsRouter    = require('./routes/ProductsRouter');
+
+app.use(express.json());
+app.use(express.urlencoded({extended : true}));
+app.use(express.static(path.join(__dirname,('public'))));
+app.use(cookieParser());
+app.set('view engine','ejs');
+
+
+app.use('/owners'   , ownersRouter);
+app.use('/users'    , usersRouter);
+app.use('/products' , productsRouter);
+
+app.get('/', (req,res)=>{
+    res.send('hey');
+});
+
+app.listen(3000);
---
+++ models/owner-model.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const ownerSchema =  mongoose.Schema({
+    fullname: {
+        type      : String,
+        minlength : 3,
+    },
+    email   : String,
+    password: String,
+    cart    : {
+        type: Array,
+        default : [],
+    },
+    products: {
+       type : Array,
+       default : [],
+    },
+    picture : String,
+    gstin   : Number
+});
+
+module.exports = mongoose.model('owner',ownerSchema);
