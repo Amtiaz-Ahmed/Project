1488cf8f2be96d470e496f631417239d944eb793
Amtiaz-Ahmed <amtiazahmed797@gmail.com>
2024-06-26 12:34:51 +0500

    EJs updated

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const express           = require('express');
 const app               = express();
 const path              = require('path');
 const cookieParser      = require('cookie-parser');
-
+const expressSession    = require('express-session');
+const flash             = require('connect-flash');
 const db                = require('./config/mongoose-connection');
 const usersRouter       = require('./routes/usersRouter');
 const ownersRouter      = require('./routes/ownersRouter');
 const productsRouter    = require('./routes/ProductsRouter');
-const indexRouter    = require('./routes/indexRouter');
+const indexRouter       = require('./routes');
+const isLoggedIn = require('./middleware/isLoggedIn');
 
 require("dotenv").config();
 
@@ -15,15 +17,30 @@ app.use(express.json());
 app.use(express.urlencoded({extended : true}));
 app.use(express.static(path.join(__dirname,('public'))));
 app.use(cookieParser());
-app.set('view engine','ejs');
 
+app.use(
+    expressSession({
+        resave: false,
+        saveUninitialized : false,
+        secret : process.env.EXPRESS_SESSION_SECRET,
+    })
+);
+app.use(flash());
+app.set('view engine','ejs');
 
 app.use('/owners'   , ownersRouter);
 app.use('/users'    , usersRouter);
 app.use('/products' , productsRouter);
 
 app.get('/', (req,res)=>{
-    res.send('hey');
+    let error =  req.flash("error")
+    res.render('index',{error});
 });
+
+app.get('/shop',isLoggedIn, (req,res)=>{
+    res.render('shop');
+});
+
+
 
 app.listen(3000);
---
+++ routes/index.js
@@ -0,0 +1,12 @@
+const express     = require('express');
+const router      = express.Router();
+const isLoggedIn = require('../middleware/isLoggedIn'); 
+
+router.get('/',(req,res)=>{
+    let error = req.flash("error");
+    res.render('index',{ error })
+});
+
+router.get('/shop',isLoggedIn,(req,res)=>{
+    res.render('shop');
+})
